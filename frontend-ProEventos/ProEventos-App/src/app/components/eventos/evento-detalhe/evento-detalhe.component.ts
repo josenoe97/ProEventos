5704d56e1f5dd4fcd275c8f616dbc4bedd4dd6c6
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-11-30 21:05:23 -0300

    configurando evento-detalhe

--- frontend-ProEventos/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ frontend-ProEventos/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./evento-detalhe.component.scss']
 })
 export class EventoDetalheComponent implements OnInit {
-
+  
+  form: FormGroup | any;
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.validation();
   }
-
+  
+  public validation(): void{
+    this.form = new FormGroup({
+      tema: new FormControl('', 
+        [Validators.required, Validators.minLength(4), Validators.maxLength(50)]), 
+      local: new FormControl('xpto', Validators.required), 
+      dataEvento: new FormControl('xpto', Validators.required), 
+      qtdPessoas: new FormControl('xpto', 
+        [Validators.required, Validators.maxLength(120000)]), 
+      imagemURL: new FormControl('xpto', Validators.required), 
+      telefone: new FormControl('xpto', Validators.required), 
+      email: new FormControl('xpto',
+        [Validators.required, Validators.email]),  
+    });
+  }
 }
